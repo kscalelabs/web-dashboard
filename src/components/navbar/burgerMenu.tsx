fa3e2eb7e858a8242cff5d24c959d516f9250676
Deborah Wang <dqw@sfu.ca>
2024-12-14 23:28:52 -0800

    upadte nav and footer

--- src/components/navbar/burgerMenu.tsx
+++ src/components/navbar/burgerMenu.tsx
@@ -8,40 +8,42 @@ import { motion } from "motion/react";
 
 const BurgerMenu = (isOpen: boolean) => {
   return isOpen ? (
-    <motion.div
-      className={"flex flex-col col-span-full list-none justify-evenly h-[100dvh]"}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.2, ease: "circOut" }}
-    >
-      {navItemsMobile.map((navItem, index) => {
-        return (
-          <motion.button
-            key={index}
-            className={
-              "col-span-full font-planar font-normal capitalize text-4xl select-none flex flex-row text-foreground w-fit pointer-events-auto" +
-              " hover:text-rust " +
-              transitionEaseLinearDuration300
-            }
-            initial="initial"
-            whileHover="hover"
-            aria-label="Menu"
-          >
-            <a
-              className={"font-planar pointer-events-auto"}
-              href={navItemLinksMobile[index].link}
-              target={navItemLinksMobile[index].target}
-            >
-              {navItem}
-            </a>
-            {index !== 1 ? <ExpressiveArrow size={"size-10"} /> : <></>}
-          </motion.button>
-        );
-      })}
-    </motion.div>
+      <div className={"grid-m"}>
+        <motion.div
+            className={"flex flex-col col-span-full list-none justify-evenly h-[100dvh]"}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.2, ease: "circOut"}}
+        >
+          {navItemsMobile.map((navItem, index) => {
+            return (
+                <motion.button
+                    key={index}
+                    className={
+                        "col-span-full font-planar font-normal capitalize text-4xl select-none flex flex-row text-foreground w-fit pointer-events-auto" +
+                        " hover:text-rust " +
+                        transitionEaseLinearDuration300
+                    }
+                    initial="initial"
+                    whileHover="hover"
+                    aria-label="Menu"
+                >
+                  <a
+                      className={"font-planar pointer-events-auto"}
+                      href={navItemLinksMobile[index].link}
+                      target={navItemLinksMobile[index].target}
+                  >
+                    {navItem}
+                  </a>
+                  {index !== 1 ? <ExpressiveArrow size={"size-10"}/> : <></>}
+                </motion.button>
+            );
+          })}
+        </motion.div>
+      </div>
   ) : (
-    <></>
+      <></>
   );
 };
 
--- src/components/navbar/burgerOpenButton.tsx
+++ src/components/navbar/burgerOpenButton.tsx
@@ -5,60 +5,60 @@ import { motion } from "motion/react";
 const BurgerOpenButton = ({ ...props }: BurgerOpenButtonProps) => {
   return (
     <button
-      className={clsx("cursor-pointer h-fit", props.className)}
+      className={clsx("cursor-pointer h-fit p-1 ", props.className)}
       onClick={() => {
         props.onClick(!props.isOpen);
       }}
     >
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="size-6 translate-y-1"
-      >
-        <motion.rect
-          x="3"
-          y="6"
-          width="18"
-          height="2"
-          initial={{
-            rotate: 0,
-            y: 0,
-            fill: "var(--foreground)",
-          }}
-          animate={{
-            rotate: props.isOpen ? 15 : 0,
-            y: props.isOpen ? -2 : 0,
-            fill: "var(--foreground)",
-          }}
-        />
-        <motion.rect
-          x="3"
-          y="11"
-          width="18"
-          height="2"
-          initial={{
-            fill: "var(--foreground)",
-          }}
-          animate={{
-            fill: "var(--foreground)",
-          }}
-        />
-        <motion.rect
-          x="3"
-          y="16"
-          width="18"
-          height="2"
-          initial={{
-            fill: "var(--foreground)",
-          }}
-          animate={{
-            fill: "var(--foreground)",
-          }}
-        />
-      </svg>
+            <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-6 translate-y-1"
+            >
+                <motion.rect
+                    x="3"
+                    y="6"
+                    width="18"
+                    height="2"
+                    initial={{
+                        rotate: 0,
+                        y: 0,
+                        fill: "var(--foreground)",
+                    }}
+                    animate={{
+                        rotate: props.isOpen ? 15 : 0,
+                        y: props.isOpen ? -2 : 0,
+                        fill: "var(--foreground)",
+                    }}
+                />
+                <motion.rect
+                    x="3"
+                    y="11"
+                    width="18"
+                    height="2"
+                    initial={{
+                        fill: "var(--foreground)",
+                    }}
+                    animate={{
+                        fill: "var(--foreground)",
+                    }}
+                />
+                <motion.rect
+                    x="3"
+                    y="16"
+                    width="18"
+                    height="2"
+                    initial={{
+                        fill: "var(--foreground)",
+                    }}
+                    animate={{
+                        fill: "var(--foreground)",
+                    }}
+                />
+            </svg>
     </button>
   );
 };
